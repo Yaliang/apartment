92abd57c03ffe7263e2f5311e08743cb16b51a86
Yaliang Wang <yaliangwang2015@u.northwestern.edu>
2015-11-21 13:58:01 -0600

    add empty framework for an app

---
+++ index.js
@@ -0,0 +1,77 @@
+Parse.initialize("3fbKPbURkYVceP1d95UpCt2KTTHgl0U0LjXDXwYw", "T9i1AvuIWIz4vpobQYy0qZpV0KIaJwKsjxg5B5f6");
+
+ajaxloader = {
+	callback: false,
+	/**
+	 * the entry to request a get function
+	 * @param  {String} id The id of file, it normally comes with the form "./<id>.html"
+	 * @return {[type]}    [description]
+	 */
+	get : function(id) {
+		var href = "./pages/"+id+".html"
+		ajaxloader.done = false
+		$.get(href, function(data) {
+			ajaxloader.extract(data)
+			ajaxloader.done = true
+		})
+	},
+	/**
+	 * the function to extract the packaged file
+	 * @param  {String} data The content of the file
+	 * @return {Object}      The unpackaged object
+	 */
+	extract : function(data) {
+
+		var obj = {}
+
+		var pageid = data.substring(data.indexOf("<pageid>") + 8, data.indexOf("</pageid>"))
+		if (pageid != null) {
+			obj.pageid = pageid
+			obj.pageId = pageid
+		} else {
+			obj.pageid = ""
+			obj.pageId = ""
+		}
+
+		var title = data.substring(data.indexOf("<title>") + 7, data.indexOf("</title>"))
+		if (title != null) {
+			obj.title = title
+		} else {
+			obj.title = ""
+		}
+
+		var header = data.substring(data.indexOf("<header>") + 8, data.indexOf("</header>"))
+		if (header != null) {
+			obj.header = header
+		} else {
+			obj.header = ""
+		}
+
+		var main = data.substring(data.indexOf("<main>") + 6, data.indexOf("</main>"))
+		if (main != null) {
+			obj.main = main
+			obj.content = main
+		} else {
+			obj.main = ""
+			obj.content = ""
+		}
+
+		obj.contentAttr = {}
+		var contentAttrOverscroll = data.substring(data.indexOf("<contentAttrOverscroll>") + 23, data.indexOf("</contentAttrOverscroll>"))
+		if (contentAttrOverscroll != null) {
+			obj.contentAttr.overscroll = contentAttrOverscroll == "true"
+		}
+
+		var footer = data.substring(data.indexOf("<footer>") + 8, data.indexOf("</footer>"))
+		if (footer != null) {
+			obj.footer = footer
+		} else {
+			obj.footer = ""
+		}
+
+
+		this.obj = obj
+
+		return obj
+	},
+}
